18fff2d3e0811b024902970642ea72194dbad6e7
RITWIK MISHRA <ritwikmishrastudent@gmail.com>
2025-03-13 09:51:52 +0530

    tw

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,15 +3,9 @@ export default {
   content: [
     "./index.html",
     "./src/**/*.{js,jsx,ts,tsx}",
-    "./src/components/**/*.{js,jsx,ts,tsx}",
-    "./src/App.{js,jsx,ts,tsx}"
   ],
   theme: {
     extend: {},
   },
   plugins: [],
-  future: {
-    removeDeprecatedGapUtilities: true,
-    purgeLayersByDefault: true,
-  },
 } 
