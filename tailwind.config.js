4a4a898ef77b144659a03d33d94c316dcb1cd198
RITWIK MISHRA <ritwikmishrastudent@gmail.com>
2025-03-13 09:56:52 +0530

    tw update

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ export default {
   content: [
     "./index.html",
     "./src/**/*.{js,jsx,ts,tsx}",
+    "./src/components/**/*.{js,jsx,ts,tsx}",
+    "./src/pages/**/*.{js,jsx,ts,tsx}",
+    "./src/App.{js,jsx,ts,tsx}"
   ],
   theme: {
     extend: {},
